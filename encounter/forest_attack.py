89a29dee5ba8276cdf7c187ccbb867ec0771837e
John Soza <johnsoza1988@gmail.com>
2026-02-26 15:42:55 -0800

    Add forest_attack by John Soza

--- encounter/forest_attack.py
+++ encounter/forest_attack.py
@@ -8,7 +8,7 @@
 #  Description: Player is attacked by wolves and loses a random amount 
 #  of health.
 #
-#  Author: <Your Name>
+#  Author: <John Michael Soza>
 #
 # ------------------------------------------------------------------------
 
@@ -16,4 +16,21 @@ import random
 
 def forest_attack(health):
     print("Forest Attack!")
-    return health-5
+    damage = random.randint(0,5)
+    print("A feral pack of starving wolves assail you from the bushes!\nThe foremost wolf lunges at you baring it's saliva-imbued fangs!")
+
+    if damage == 0:
+        health = health
+        print("The wolf missed it's mark! You remain unharmed!")
+    else:
+        health = health - damage
+        print(f"You suffered {damage} points of damage!")
+
+    if health > 0:
+        print(f"Your remaining health is {health}")
+    else:
+        print("The jagged fangs tore into your throat!\nThe wolves gorge themselves on your lifeless corpse.")
+
+    return health
+
+
