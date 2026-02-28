a3e2a23f61850dc911c1ce73d6de1b0d4f38b678
Victoria Coleman <toryecoleman@gmail.com>
2026-02-28 17:10:13 +0000

    Added healing_herbs by Tory

--- encounter/healing_herbs.py
+++ encounter/healing_herbs.py
@@ -8,12 +8,33 @@
 #  Description: Player discovers healing herbs that restore a small
 #    amount of health.
 #
-#  Author: <Your Name>
+#  Author: Victoria Coleman
 #
 # ------------------------------------------------------------------------
 
 import random
 
 def healing_herbs(health):
-    print("Healing Herbs")
-    return health+5
+    """
+    The player discovers a random healing herb that restores a small amount of
+    health.
+    """
+
+    #Choose a random herb
+    herb = random.randint(1,3)
+
+    if herb == 1:
+        print("You find some lavender.\nYou feel much calmer now.")
+        #add a random amount of health
+        health += random.randint(2, 3)
+
+    elif herb == 2:
+        print("You find some peppermint.\nYou're stomach is settled.")
+        health += random.randint(3,4)
+
+    else:
+        print("You find some aloe.\nIt soothed your skin.")
+        health += random.randint(4,5)
+
+    return health
+
