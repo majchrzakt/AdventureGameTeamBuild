64ade861ab6d63eabf63beeb6daf58512c04622f
Shylsomer <pulmonoscorpius70@gmail.com>
2026-03-02 16:09:08 -0800

    Add find_treasure by Torin

--- wealth/find_treasure.py
+++ wealth/find_treasure.py
@@ -7,12 +7,15 @@
 #
 #  Description: Player finds treasure and gains a random amount of gold.
 #
-#  Author: <Your Name>
+#  Author: Torin
 #
 # ------------------------------------------------------------------------
 
-import random
+from random import randint
 
 def find_treasure(gold):
-    print("Find Treasure")
-    return gold+5
+    tv = gold + randint(8,29) #tv = total value
+    print("In a small crevice, you catch a glimpse of a weary vessel, long\n"
+          "forgotten by time. Reaching in, you find your hand grasping an\n"
+          "old, tired handle. You gasp! It's full of gems and curiosities!")
+    return tv
